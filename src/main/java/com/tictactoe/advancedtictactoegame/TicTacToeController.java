275c82c1c58861d53849f8d176380c23e6e784dd
alans <alan.sipiorski14@gmail.com>
2022-07-03 14:11:29 +0200

    make sample game screen fxml

---
+++ src/main/java/com/tictactoe/advancedtictactoegame/TicTacToeController.java
@@ -0,0 +1,71 @@
+package com.tictactoe.advancedtictactoegame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TicTacToeController {
+
+    @FXML
+    private Button b1;
+
+    @FXML
+    private Button b2;
+
+    @FXML
+    private Button b3;
+
+    @FXML
+    private Button b4;
+
+    @FXML
+    private Button b5;
+
+    @FXML
+    private Button b6;
+
+    @FXML
+    private Button bottomLeftButton;
+
+    @FXML
+    private Button bottomMiddleButton;
+
+    @FXML
+    private Button bottomRightButton;
+
+    @FXML
+    private Button middleCenterButton;
+
+    @FXML
+    private Button middleLeftButton;
+
+    @FXML
+    private Button middleRightButton;
+
+    @FXML
+    private Button r1;
+
+    @FXML
+    private Button r2;
+
+    @FXML
+    private Button r3;
+
+    @FXML
+    private Button r4;
+
+    @FXML
+    private Button r5;
+
+    @FXML
+    private Button r6;
+
+    @FXML
+    private Button topLeftButton;
+
+    @FXML
+    private Button topMiddleButton;
+
+    @FXML
+    private Button topRightButton;
+
+}
