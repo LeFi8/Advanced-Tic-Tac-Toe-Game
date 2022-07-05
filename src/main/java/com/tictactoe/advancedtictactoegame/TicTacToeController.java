57c55f714a61dde302612d39a4b5ee2ca3ff6362
alans <alan.sipiorski14@gmail.com>
2022-07-05 14:49:26 +0200

    finish visuals in fxml file

--- src/main/java/com/tictactoe/advancedtictactoegame/TicTacToeController.java
+++ src/main/java/com/tictactoe/advancedtictactoegame/TicTacToeController.java
@@ -1,36 +1,31 @@
 package com.tictactoe.advancedtictactoegame;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class TicTacToeController {
 
     @FXML
-    private Button b1;
+    private Text centerText;
 
     @FXML
-    private Button b2;
+    private Button topLeftButton;
 
     @FXML
-    private Button b3;
+    private Button topMiddleButton;
 
     @FXML
-    private Button b4;
-
-    @FXML
-    private Button b5;
-
-    @FXML
-    private Button b6;
-
-    @FXML
-    private Button bottomLeftButton;
-
-    @FXML
-    private Button bottomMiddleButton;
-
-    @FXML
-    private Button bottomRightButton;
+    private Button topRightButton;
 
     @FXML
     private Button middleCenterButton;
@@ -40,6 +35,15 @@ public class TicTacToeController {
 
     @FXML
     private Button middleRightButton;
+
+    @FXML
+    private Button bottomLeftButton;
+
+    @FXML
+    private Button bottomMiddleButton;
+
+    @FXML
+    private Button bottomRightButton;
 
     @FXML
     private Button r1;
@@ -60,12 +64,76 @@ public class TicTacToeController {
     private Button r6;
 
     @FXML
-    private Button topLeftButton;
+    private Button b1;
 
     @FXML
-    private Button topMiddleButton;
+    private Button b2;
 
     @FXML
-    private Button topRightButton;
+    private Button b3;
+
+    @FXML
+    private Button b4;
+
+    @FXML
+    private Button b5;
+
+    @FXML
+    private Button b6;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private AnchorPane root;
+
+    private int playersTurn;
+
+    @FXML
+    void onStart(ActionEvent event) {
+        System.out.println("Start");
+        playersTurn = (int) Math.round(Math.random());
+        centerText.setText(nextTurn());
+    }
+
+    @FXML
+    void onReset() throws IOException {
+        Scene scene = root.getScene();
+        scene.setRoot(
+                FXMLLoader.load(
+                        Objects.requireNonNull(
+                                GameApp.class.getResource("tic-tac-toe.fxml")
+                        )
+                ));
+    }
+
+    void clearButtons(Button button){
+        button.setText("");
+    }
+
+    @FXML
+    void onClick(ActionEvent event) {
+        Button current = (Button)event.getSource();
+        if (current.getTextFill().equals(Color.GRAY))
+            return;
+
+        topLeftButton.setOnAction(e -> {
+            topLeftButton.setText(current.getText());
+            topLeftButton.setTextFill(current.getTextFill());
+            topLeftButton.setFont(new Font(30));
+            current.setTextFill(Color.GRAY);
+        });
+
+        System.out.println("Clicked button " + current.getId());
+    }
+
+    private String nextTurn(){
+        if (playersTurn % 2 == 0)
+            return "Blue player's turn";
+        else return "Red player's turn";
+    }
 
 }
