baa2e20be0f10bf96fff6a9c4d5e29ee66ff3c2f
alans <alan.sipiorski14@gmail.com>
2022-07-06 15:21:49 +0200

    add turns changing

--- src/main/java/com/tictactoe/advancedtictactoegame/TicTacToeController.java
+++ src/main/java/com/tictactoe/advancedtictactoegame/TicTacToeController.java
@@ -11,6 +11,9 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class TicTacToeController {
@@ -82,21 +85,41 @@ public class TicTacToeController {
     private Button b6;
 
     @FXML
-    private Button resetButton;
-
-    @FXML
-    private Button startButton;
-
-    @FXML
     private AnchorPane root;
+
+    @FXML
+    private Text redPlayer;
+
+    @FXML
+    private Text bluePlayer;
 
     private int playersTurn;
 
+    private final List<Button> allFields = new ArrayList<>();
+
+    private final List<Button> redPlayerButtons = new ArrayList<>();
+
+    private final List<Button> bluePlayerButtons = new ArrayList<>();
+
     @FXML
-    void onStart(ActionEvent event) {
-        System.out.println("Start");
+    void initialize() {
+        allFields.addAll(Arrays.asList(
+                topLeftButton, topMiddleButton, topRightButton,
+                middleLeftButton, middleCenterButton, middleRightButton,
+                bottomLeftButton, bottomMiddleButton, bottomRightButton
+        ));
+
+        redPlayerButtons.addAll(Arrays.asList(r1, r2, r3, r4, r5, r6));
+        bluePlayerButtons.addAll(Arrays.asList(b1, b2, b3, b4, b5, b6));
+
+        changeButtonsState(redPlayerButtons, false);
+        changeButtonsState(bluePlayerButtons, false);
+    }
+
+    @FXML
+    void onStart() {
         playersTurn = (int) Math.round(Math.random());
-        centerText.setText(nextTurn());
+        nextTurn();
     }
 
     @FXML
@@ -107,33 +130,77 @@ public class TicTacToeController {
                         Objects.requireNonNull(
                                 GameApp.class.getResource("tic-tac-toe.fxml")
                         )
-                ));
-    }
-
-    void clearButtons(Button button){
-        button.setText("");
+                )
+        );
     }
 
     @FXML
     void onClick(ActionEvent event) {
-        Button current = (Button)event.getSource();
-        if (current.getTextFill().equals(Color.GRAY))
+        Button currentValue = (Button)event.getSource();
+        if (currentValue.getTextFill().equals(Color.GRAY))
             return;
 
-        topLeftButton.setOnAction(e -> {
-            topLeftButton.setText(current.getText());
-            topLeftButton.setTextFill(current.getTextFill());
-            topLeftButton.setFont(new Font(30));
-            current.setTextFill(Color.GRAY);
-        });
-
-        System.out.println("Clicked button " + current.getId());
+        chooseField(currentValue, topLeftButton);
+        chooseField(currentValue, topMiddleButton);
+        chooseField(currentValue, topRightButton);
+        chooseField(currentValue, middleLeftButton);
+        chooseField(currentValue, middleCenterButton);
+        chooseField(currentValue, middleRightButton);
+        chooseField(currentValue, bottomLeftButton);
+        chooseField(currentValue, bottomMiddleButton);
+        chooseField(currentValue, bottomRightButton);
     }
 
-    private String nextTurn(){
-        if (playersTurn % 2 == 0)
-            return "Blue player's turn";
-        else return "Red player's turn";
+    //TODO: other winning patterns
+    private boolean checkForWinner(){
+        if (!topLeftButton.getText().isEmpty() && !topMiddleButton.getText().isEmpty() && !topRightButton.getText().isEmpty()) {
+            if (topLeftButton.getTextFill().equals(topMiddleButton.getTextFill())
+                    && topMiddleButton.getTextFill().equals(topRightButton.getTextFill())) {
+                centerText.setText("Winner!");
+                centerText.setFill(topLeftButton.getTextFill());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void chooseField(Button value, Button field){
+        field.setOnAction(e -> {
+            if (!field.getText().isEmpty())
+                if (Integer.parseInt(value.getText()) <= Integer.parseInt(field.getText()))
+                    return;
+
+            if (value.getTextFill().equals(Color.GRAY))
+                return;
+
+            field.setText(value.getText());
+            field.setTextFill(value.getTextFill());
+            field.setFont(new Font(30));
+            value.setTextFill(Color.GRAY);
+            nextTurn();
+        });
+    }
+
+    private void nextTurn(){
+        if (checkForWinner()) return;
+
+        playersTurn++;
+        if (playersTurn % 2 == 0) {
+            changeButtonsState(redPlayerButtons, false);
+            changeButtonsState(bluePlayerButtons, true);
+            centerText.setText("Blue player's turn");
+            centerText.setFill(bluePlayer.getFill());
+        } else {
+            changeButtonsState(bluePlayerButtons, false);
+            changeButtonsState(redPlayerButtons, true);
+            centerText.setText("Red player's turn");
+            centerText.setFill(redPlayer.getFill());
+        }
+    }
+
+    private void changeButtonsState(List<Button> player, boolean state){
+        for (Button button : player)
+            button.setDisable(!state);
     }
 
 }
